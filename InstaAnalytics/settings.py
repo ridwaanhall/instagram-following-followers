8d19f6dc9724bff548b8fae904d81e8ddd65e7dc
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-02-08 18:05:38 +0700

    feat: update settings for static file handling and remove media URL configuration

--- InstaAnalytics/settings.py
+++ InstaAnalytics/settings.py
@@ -128,8 +128,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
