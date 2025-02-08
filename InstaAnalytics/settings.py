2c27274a6a073a3bb79fb019e65fcdf0f40a3600
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-02-08 18:10:32 +0700

    feat: add Whitenoise for static file serving in settings

--- InstaAnalytics/settings.py
+++ InstaAnalytics/settings.py
@@ -38,6 +38,8 @@ else:
 # Application definition
 
 INSTALLED_APPS = [
+    "whitenoise.runserver_nostatic",
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -50,6 +52,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
